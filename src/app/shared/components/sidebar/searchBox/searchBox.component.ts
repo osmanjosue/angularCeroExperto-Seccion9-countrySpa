4f8d63fd9802500d71be0d97e74a7e837e4b9065
osmanjosue <osmanjosue@gmail.com>
2024-03-12 10:15:27 -0600

    Seccion 10 => Country spa => Debounce - Peticiones cuando el usuario deja de escribir

--- src/app/shared/components/sidebar/searchBox/searchBox.component.ts
+++ src/app/shared/components/sidebar/searchBox/searchBox.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime, pipe } from 'rxjs';
 
 @Component({
   selector: 'shared-searchBox',
@@ -7,20 +8,37 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class SearchBoxComponent implements OnInit {
 
+  private debouncer = new Subject<string>;
+  // private debouncer: Subject<string> = new Subject<string>;
+
   @Input()
   public placeholder: string = '';
 
   @Output()
   public onValue = new EventEmitter<string>();
   // public onValue: EventEmitter<string> = new EventEmitter<string>();
+  @Output()
+  public onDebounce = new EventEmitter<string>();
 
-  emitValue( value: string): void {
-    this.onValue.emit( value );
+  ngOnInit() {
+    this.debouncer
+      .pipe(
+        debounceTime(300)
+      )
+      .subscribe(value => {
+        this.onDebounce.emit(value)
+      })
+  }
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
   }
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
