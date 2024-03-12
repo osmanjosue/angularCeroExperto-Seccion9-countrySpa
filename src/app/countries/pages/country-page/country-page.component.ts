6250fe29a1243ad3d8c05d4d36444de16f8b2633
osmanjosue <osmanjosue@gmail.com>
2024-03-11 21:56:00 -0600

    Seccion 9 => Country spa => Mostrar informacion del pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { pipe, switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { pipe, switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
 
   constructor(private activatedRoute: ActivatedRoute,
               private router: Router,
@@ -21,11 +24,8 @@ export class CountryPageComponent implements OnInit {
         switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))
       )
       .subscribe(country => {
-        if(!country) {
-          return this.router.navigateByUrl('');
-        }
-        console.log('TENEMOS UN PAIS')
-        return;
+        if(!country) return this.router.navigateByUrl('');
+        return this.country= country;
       });
   }
 
